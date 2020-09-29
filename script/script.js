8937a46ab4ed0f5fd000cbceff56d80762930ec9
Daniil <daniil.tyan.2001@mail.ru>
2020-09-29 11:56:25 +0300

    calculator

--- script/script.js
+++ script/script.js
@@ -1,15 +1,16 @@
 function calculator(expression) {
-  expression = expression.replace(/ /g, "");
+  let result = expression;
+  result = expression.replace(/ /g, "");
   let numbers = new Array();
   let numbersKey = 0;
   let symbols = new Array();
   let symbolsKey = 0;
   let tempString = "";
-  for (let i = 0; i < expression.length; i++) {
-    if (!isNaN(parseInt(expression[i]))) {
-      tempString += expression[i];
+  for (let i = 0; i < result.length; i++) {
+    if (!isNaN(parseInt(result[i]))) {
+      tempString += result[i];
     } else {
-      symbols[symbolsKey] = expression[i];
+      symbols[symbolsKey] = result[i];
       symbolsKey++;
       numbers[numbersKey] = parseInt(tempString);
       tempString = "";
@@ -17,39 +18,54 @@ function calculator(expression) {
     }
   }
   numbers[numbersKey] = parseInt(tempString);
-  //console.log(numbers);
-  //console.log(symbols);
-  console.log("___");
-  while (symbols.length != 0){
-    console.log(symbols);
-  console.log(numbers);
-  if (symbols[i] == "+") {
-    let tempNumber = numbers[i] + numbers[i + 1];
-    //console.log(tempNumber);
-    numbers[i] = tempNumber;
-  } else if (symbols[i] == "-") {
-    let tempNumber = numbers[i] - numbers[i + 1];
-    numbers[i] = tempNumber;
+  // умножение/деление
+  for (let i = 0; i < symbols.length; i++) {
+    if (symbols[i] == "*") {
+      let tempNumber = numbers[i] * numbers[i + 1];
+      numbers[i] = tempNumber;
+    } else if (symbols[i] == "/") {
+      let tempNumber = numbers[i] / numbers[i + 1];
+      numbers[i] = tempNumber;
+    }
+    if (symbols[i] == "*" || symbols[i] == "/") {
+      for (let j = i; j < numbers.length - 1; j++) {
+        numbers[j + 1] = numbers[j + 2];
+      }
+      numbers.pop();
+      for (let j = i; j < symbols.length; j++) {
+        symbols[j] = symbols[j + 1];
+      }
+      symbols.pop();
+      i--;
+    }
+
   }
-  for (let j = i; j < numbers.length; j++) {
-    numbers[i + 1] = numbers[i + 2];
+  // сложение/вычитание
+  for (let i = 0; i < symbols.length; i++) {
+    if (symbols[i] == "+") {
+      let tempNumber = numbers[i] + numbers[i + 1];
+      numbers[i] = tempNumber;
+    } else if (symbols[i] == "-") {
+      let tempNumber = numbers[i] - numbers[i + 1];
+      numbers[i] = tempNumber;
+    }
+    for (let j = i; j < numbers.length - 1; j++) {
+      numbers[j + 1] = numbers[j + 2];
+    }
+    numbers.pop();
+    for (let j = i; j < symbols.length; j++) {
+      symbols[j] = symbols[j + 1];
+    }
+    symbols.pop();
+    i--;
   }
-  for (let j = i; j < symbols.length; j++) {
-    symbols[i + 1] = symbols[i + 2];
-  }
-  numbers.pop();
-  symbols.pop();
-  console.log(symbols);
-  console.log(numbers);
-  console.log("___");
-  //i--;
-}
-return numbers;
+  result = numbers[0];
+  return result;
 }
 
 var button = document.getElementsByClassName("button")[0];
 button.addEventListener("click", function() {
   let textBox = document.getElementsByClassName("textbox")[0];
   let calculationString = textBox.value;
-  console.log(calculator(calculationString));
+  calculator(calculationString);
 });
